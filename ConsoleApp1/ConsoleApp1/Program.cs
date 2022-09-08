b9986c175762a37d9ae4eaa15ce909f9d82f3b06
LillyRay <87969226+LillyRay@users.noreply.github.com>
2022-09-08 11:20:03 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Lab1_1;
+
+class Program
+{
+    static void Main()
+    {
+        Rectangle rec = new Rectangle();
+        rec.Set_a(rec.Input(1));
+        rec.Set_b(rec.Input(2));
+        rec.Result(rec.Get_a(), rec.Get_b());
+    
+    }
+
+   
+}
+
---
+++ ConsoleApp1/ConsoleApp1/Rectangle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_1
+{
+    public class Rectangle
+    {
+        private double a_;
+        private double b_;
+        public double Get_a() { return a_; } 
+        public void Set_a(double a) 
+        {
+            if (a > 0)
+            {
+                a_ = a;
+            }
+            else
+            {
+                Console.WriteLine("Сторона може бути тiльки додатним числом.");
+                Set_a(Input(1));
+            }
+        }
+
+
+        public double Get_b() { return b_; }
+        public void Set_b(double b) 
+        {
+            if (b > 0)
+            {
+                b_ = b;
+            }
+            else
+            {
+                Console.WriteLine("Сторона може бути тiльки додатним числом.");
+                Set_b(Input(2));
+            }
+        }
+
+        public double Input(int number)
+        {
+            switch(number){
+                case 1: Console.WriteLine("Задайте першу сторону прямокутника:");
+                    break;
+                case 2:
+                    Console.WriteLine("Задайте другу сторону прямокутника:");
+                    break;
+            }
+          
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void Result(double a, double b) 
+        {
+            Console.WriteLine("Площа прямокутника:");
+            Console.WriteLine(a * b);
+            Console.WriteLine("Периметр прямокутника:");
+            Console.WriteLine((a * 2) + (b * 2));
+        }
+
+    }
+}
